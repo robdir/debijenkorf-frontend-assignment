dc67ada23b65661c19d30ccdc9e6fb805d330986
robdir <rhwickham@gmail.com>
2018-03-19 15:31:02 +0100

    initial components created

---
+++ src/Container.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './styles/container.css'
+
+class Container extends Component {
+    render() {
+        return (
+            <div className="container">
+                
+            </div>
+
+        );
+    }
+}
+
+export default Container;
---
+++ src/searchBox.js
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react'
+
+class SearchBox extends PureComponent {
+    render() {
+        return (
+            <div className="searchbox">
+                <p> Zoeken </p>
+            </div>
+        )
+    }
+}
+
+export default SearchBox
