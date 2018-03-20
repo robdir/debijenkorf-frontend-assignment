273997322a3c244c287978d0d320d7559f56dc4e
robdir <rhwickham@gmail.com>
2018-03-20 14:43:30 +0100

    excercise one completed

--- src/Container.js
+++ src/Container.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import SearchBox from './searchBox'
 import './styles/container.css'
 
 class Container extends Component {
     render() {
         return (
             <div className="container">
-                
+                <SearchBox/>
             </div>
 
         );
--- src/searchBox.js
+++ src/searchBox.js
@@ -1,10 +1,18 @@
 import React, {PureComponent} from 'react'
+import './styles/searchbox.css'
 
 class SearchBox extends PureComponent {
     render() {
         return (
-            <div className="searchbox">
-                <p> Zoeken </p>
+            <div className="searchcontainer">
+                <div className="search">
+                    <input className="input"
+                    type="text"
+                    placeholder="Zoeken"/>
+                    <img className="glass"
+                     src="https://image.flaticon.com/icons/png/128/34/34202.png"
+                     alt="search icon"/>
+                </div>
             </div>
         )
     }
