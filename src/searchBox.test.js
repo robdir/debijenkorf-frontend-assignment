8db6639ee97735fcff7c82bec202fd3d4b09d13b
robdir <rhwickham@gmail.com>
2018-03-20 15:09:29 +0100

    devDependencies

---
+++ src/searchBox.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBox from './searchBox';
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<SearchBox/>, div);
+    })
