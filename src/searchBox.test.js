67b430ad6fdb8fabe6e1827360de9397cc712eb0
robdir <rhwickham@gmail.com>
2018-03-20 16:49:49 +0100

    testing troubles

--- src/searchBox.test.js
+++ src/searchBox.test.js
@@ -4,13 +4,19 @@ import SearchBox from './searchBox';
 import Enzyme from 'enzyme'
 import { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-15'
+import sinon from 'sinon'
 
     Enzyme.configure({ adapter: new Adapter() })
 
     describe('<SearchBox>', () => {
         const box = mount(<SearchBox/>)
+        const input = box.find('input')
 
-        it ('renders!', () => {
+        it ('renders without crashing', () => {
             expect(box).toExist
+        })
+
+        it ('focuses on input', () => {
+            expect(input).toBe(document.activeElement)
         })
     })
